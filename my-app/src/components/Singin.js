45458c9e91fa97554ae2f51d325dbe85177efbf9
gagakik <gaga.kikvilashvili@gmail.com>
2023-10-04 13:58:14 +0400

    add

--- my-app/src/components/Singin.js
+++ my-app/src/components/Singin.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import App from '../App';
 import Animation from './animation';
+import './singin.css'
 
 const Singin = () => {
     return (
@@ -8,8 +9,11 @@ const Singin = () => {
         <App/>
         <Animation>
         <div className='singin'>
+        <form className='singin-form'>
         <input type='username'/>
         <input type='password'/>
+        <button type="submit" className='singin-submit'>Submit</button>
+        </form>
         </div>
         </Animation>
         </div>
--- my-app/src/components/Singup.js
+++ my-app/src/components/Singup.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Singup = () => {
     return (
         <div className='container'>
+        
             
         </div>
     );
